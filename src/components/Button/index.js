0bc596adca0064bd64d84c0b2aa7090dbf37f70f
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2021-04-25 15:23:22 -0300

    Não abrir outra pagina

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -5,7 +5,6 @@ export default function Button({ children }) {
     <a
       href="#como-doar"
       className="md:self-start drop-shadow-lg bg-yellow p-3 rounded-lg uppercase font-bold text-center"
-      target="_blank"
     >
       {children}
     </a>
